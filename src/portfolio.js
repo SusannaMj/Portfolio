1c35ba200d74658ba362497f23d2c1ce7894eeba
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-04-24 23:38:05 +1000

    added basics for typewriter effect for headline

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,1 +1,18 @@
+var headline = document.getElementById("headline");
 
+var typewriter = new Typewriter(headline, {
+  loop: false,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(10)
+  .typeString("Hi")
+
+  .pauseFor(300)
+
+  .typeString("I am Susanna")
+  .typeString("<strong>German </span> Front-end developer")
+  .typeString("Based in Australia")
+  .pauseFor(1000)
+  .start();
